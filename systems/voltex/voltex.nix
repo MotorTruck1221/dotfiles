12c5560252eeed1b950154b076dac9bac07d6691
MotorTruck1221 <motortruck1221@protonmail.com>
2025-03-31 21:22:40 -0600

    GUH

--- systems/voltex/voltex.nix
+++ systems/voltex/voltex.nix
@@ -24,7 +24,8 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
-
+  
+  nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   hardware.bluetooth = {
