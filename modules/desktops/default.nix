e5953a74ddbd3ab395c342fd95d2b3342d70225d
MotorTruck1221 <motortruck1221@protonmail.com>
2025-04-15 20:29:31 -0600

    WHOOPS

---
+++ modules/desktops/default.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+    imports = [
+        ./audio.nix
+        ./bluetooth.nix
+        ./desktop.nix
+        ./hypr.nix
+        ./misc.nix
+        ./qt.nix
+        ./terminal.nix
+    ];
+}
