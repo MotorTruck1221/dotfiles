35f10a3a42e53cf58470281bba181286c95d8f84
MotorTruck1221 <motortruck1221@protonmail.com>
2025-06-15 23:06:08 -0600

    I love breakin stuff

--- systems/vps-1/default.nix
+++ systems/vps-1/default.nix
@@ -13,18 +13,23 @@
 
  #boot.loader.systemd-boot.enable = true;
  #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
-      enable = true;
-      device = "/dev/sda";
-  };
-
-  networking.defaultGateway = {
-    address = "104.36.84.1";
-    interface = "enp3s0";
-  };
+    boot.loader.grub = {
+        enable = true;
+        device = "/dev/sda";
+    };
+  
+    networking = {
+        interfaces.enp3s0.ipv4.addresses = [{
+            address = "104.36.87.114";
+            prefixLength =  22;	
+        }]; 
+        defaultGateway = {
+            address = "104.36.84.1";
+            interface = "enp3s0";
+        };
+        firewall.enable = false;
+    };
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  networking.firewall.enable = false; 
-  #networking.useDHCP = lib.mkDefault true;
-  system.stateVersion = "24.11";
+    system.stateVersion = "24.11";
 }
--- systems/vps-1/hardware.nix
+++ systems/vps-1/hardware.nix
@@ -27,11 +27,7 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  #networking.useDHCP = lib.mkDefault true;
-  #networking.interfaces.enp3s0.ipv4.addresses = [{
-  #address = "104.36.87.114";
-  #	prefixLength =  22;	
-  #}]; 
+  #networking.useDHCP = lib.mkDefault true; 
 
   #networking.nameservers = [ "1.1.1.1" ];
 
