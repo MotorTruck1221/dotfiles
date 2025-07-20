dbb84f08726a1e94008c6b6d7a4969705b9ee3b9
MotorTruck1221 <motortruck1221@protonmail.com>
2025-07-20 04:06:12 -0600

    Okay

--- modules/servers/haproxy.nix
+++ modules/servers/haproxy.nix
@@ -7,7 +7,7 @@
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
             Type = "simple";
-            ExecStart = "${pkgs.haproxy}/bin/haproxy -db -f /run/secrets/haproxy";
+            ExecStart = "${pkgs.haproxy}/bin/haproxy -db -f ${config.sops.secrets.haproxy.path}";
             Restart = "on-failure";
         };
     };
