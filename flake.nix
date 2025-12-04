0b22892cc0269b652b399d038062b10e02840711
MotorTruck1221 <motortruck1221@protonmail.com>
2025-12-04 01:09:44 -0700

    Maybe before updating the nix store I should actually make sure i'm on the latest

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
         url = "github:nixos/nixpkgs/nixos-unstable"; 
     };
     nixpkgs = {
-        url = "github:nixos/nixpkgs/nixos-25.05";
+        url = "github:nixos/nixpkgs/nixos-25.11";
     };
     agenix = { 
         url = "github:ryantm/agenix";
