fa3d56a6bd3e4991575961d59e0af19cabf977d0
MotorTruck1221 <motortruck1221@protonmail.com>
2025-12-07 21:56:04 -0700

    Clean some stuff up

--- modules/servers/optional/ssl.nix
+++ modules/servers/optional/ssl.nix
@@ -6,14 +6,16 @@
             email = "motortruck1221@protonmail.com";
             #server = "https://acme-staging-v02.api.letsencrypt.org/directory";
         };
-        certs."incog.works" = {
-            dnsProvider = "cloudflare";
-            environmentFile = config.sops.secrets.cloudflare-api.path;
-        };
-        certs."nebulaservices.org" = {
-            dnsProvider = "cloudflare";
-            environmentFile = config.sops.secrets.cloudflare-api.path;
-            extraDomainNames = [ "nebulaproxy.io" ];
+        certs = {
+            "incog.works" = {
+                dnsProvider = "cloudflare";
+                environmentFile = config.sops.secrets.cloudflare-api.path;
+            };
+            "nebulaservices.org" = {
+                dnsProvider = "cloudflare";
+                environmentFile = config.sops.secrets.cloudflare-api.path;
+                extraDomainNames = [ "nebulaproxy.io" "rubynetwork.co" ];
+            };
         };
     };
 }
