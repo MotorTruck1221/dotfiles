66de7286ae21b9135998b0c51159c149e0287ed3
MotorTruck1221 <motortruck1221@protonmail.com>
2026-03-02 23:40:28 -0700

    Update nebula

--- modules/servers/optional/postgres.nix
+++ modules/servers/optional/postgres.nix
@@ -2,6 +2,7 @@
 {
     services.postgresql = {
         enable = true;
+        ensureDatabases = [ "nebula" ];
         authentication = pkgs.lib.mkOverride 10 ''
             local all all trust
         '';
