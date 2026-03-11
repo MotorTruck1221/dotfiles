e8159156e401c657653804178b51f1c54ea05e49
MotorTruck1221 <motortruck1221@protonmail.com>
2026-03-11 01:29:07 -0600

    Update nebula

--- modules/servers/optional/oci-containers.nix
+++ modules/servers/optional/oci-containers.nix
@@ -11,7 +11,7 @@
                 ];
             };
             nebula = {
-                image = "motortruck1221/nebula:9.1.3";
+                image = "motortruck1221/nebula:9.1.4";
                 ports = ["6001:8080"];
                 volumes = [
                     "${config.sops.secrets.nebula.path}:/app/config.toml"
